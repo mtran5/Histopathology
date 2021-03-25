34597a3597c97d51514e5ff35894c45a777113fe
mtran5 <78325563+mtran5@users.noreply.github.com>
2021-03-24 22:46:26 -0500

    Add histopathology.ipynb

--- core/small_dataset.py
+++ core/small_dataset.py
@@ -9,8 +9,12 @@ Created on Tue Mar 23 13:30:47 2021
 import sys
 from data_utils import *
 import numpy
+from distutils.dir_util import copy_tree
 
 if __name__ == "__main__":
+    
+    # Your foldername goes here
+    # This is the folder that contains the dataset of patients
     foldername = "../../histopathology_dataset/"
     seed = 42
     
@@ -28,7 +32,7 @@ if __name__ == "__main__":
     test_patients = patients[int(len(patients) * train_test_split):]
     
     # Generate a small dataset of train data for better prototyping
-    train_patients = train_patients[:20]
+    train_patients = train_patients[:25]
     
     X_train, y_train = GetDataset.get_dataset(foldername, train_patients)
     X_test, y_test  = GetDataset.get_dataset(foldername, test_patients)
@@ -36,6 +40,14 @@ if __name__ == "__main__":
     # Get the class distribution from the small dataset
     print("Positive classes percentage in train data {:2.2%}.".format(sum(y_train)/len(y_train)))
     
-    # Using shutil to copy and paste these files into a new dataset
-    
-    
+    # Using shutil to copy and paste the small dataset
+    # CAREFUL: Only use this code once
+    '''
+    for patient in train_patients:
+        print("Copying {0}...".format(patient))
+        src_file = foldername + patient
+        dst_file = "../../histopathology_small/" + patient
+        if os.path.exists(dst_file):
+            continue
+        copy_tree(src_file, dst_file)
+    '''
